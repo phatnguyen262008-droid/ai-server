42aa336f3993d051ee66a2c82112a192c669e873
NgThePhat <phatnguyen262008@gmail.com>
2026-02-28 13:36:00 +0700

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  console.log("OPENROUTER_KEY:", process.env.OPENROUTER_KEY);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
